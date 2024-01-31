ffe1a5200e97b984728d9174740193c9b55fe694
jonwalajay123 <114734238+jonwalajay123@users.noreply.github.com>
2024-01-31 11:17:12 +0530

    restorant class filed

---
+++ Zomato_Backend/src/main/java/com/food/Restaurant.java
@@ -0,0 +1,28 @@
+package com.food;
+
+import java.time.LocalTime;
+
+public class Restaurant {
+	    private Long restaurantId;
+	    private String restaurantName;
+	    private String contactNumber;
+	    private String Address;
+	    private String latitude;
+	    private String longitude;
+	    private String email;
+	    private String contactPerson;
+	    private LocalTime openingTime;
+	    private LocalTime closingTime;
+	    private String currentStatus;
+	    private String active;
+	    private String otp;
+	    private String logo;
+	    private String city;
+	    private String state;
+	    private String pincode;
+	    private String type;
+	    private String cuision;
+	    private String description;
+
+
+}
